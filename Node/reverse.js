fdf1185f9dd54b35f6e69fb27bba6fd09cd9bbb6
DennisRono <finnneron1@gmail.com>
2022-04-07 19:48:01 +0300

    slip glass morning policeman goes

--- Node/reverse.js
+++ Node/reverse.js
@@ -19,9 +19,8 @@ let config = {
   method: 'post',
   url: 'https://sandbox.safaricom.co.ke/mpesa/reversal/v1/request',
   headers: { 
-    'Authorization': 'Bearer <Access-Token>', 
-    'Content-Type': 'application/json', 
-    'Cookie': 'incap_ses_1020_2742146=CIw9cWQ6n1Q4Y2/XPcUnDj8VT2IAAAAAxuAC2/1nq18paW/T2Lku+Q==; visid_incap_2742146=J+ZfDWoVT3uhUREl6qr84iz/TmIAAAAAQUIPAAAAAACvhu6L79mC7q80R1QNdYOw'
+    'Authorization': 'Bearer '+access_token,
+    'Content-Type': 'application/json',
   },
   data : data
 };

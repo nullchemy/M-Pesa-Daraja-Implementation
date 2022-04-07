d4df7ae468628af99fe5626490ac7926d2517291
DennisRono <finnneron1@gmail.com>
2022-04-07 19:21:00 +0300

    block half excellent cake firm

--- Node/b2b.js
+++ Node/b2b.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const access_token = require('access_token')
+
 let data = JSON.stringify({
   "Initiator": "",
   "SecurityCredential": "",
@@ -18,7 +20,7 @@ let config = {
   method: 'post',
   url: 'https://sandbox.safaricom.co.ke/mpesa/b2b/v1/paymentrequest',
   headers: { 
-    'Authorization': 'Bearer <Access-Token>', 
+    'Authorization': 'Bearer '+access_token,
     'Content-Type': 'application/json', 
   },
   data : data
